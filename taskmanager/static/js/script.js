9fab0a0356cea8b247d1af33d10e82188356f6c5
danielj314 <danieljones314@gmail.com>
2022-09-22 22:54:32 +0000

    reading or viewing tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -11,7 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
         i18n: {done: "Select"}
     });
 
-    // datepicker initialization
+    // select initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // collapsible initialization
+    let collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
+
+    
 });
